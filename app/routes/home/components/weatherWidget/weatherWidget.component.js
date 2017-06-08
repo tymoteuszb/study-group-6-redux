aa7927f4318bc4cd4b34a454bb41f42f4c061d59
Michał Kubiak <mkubiak@apptension.com>
2017-06-08 08:32:15 +0200

    Add better display of weather widget

--- app/routes/home/components/weatherWidget/weatherWidget.component.js
+++ app/routes/home/components/weatherWidget/weatherWidget.component.js
@@ -1,6 +1,6 @@
 
 import React, { PureComponent, PropTypes } from 'react';
-import {Card, CardHeader } from 'material-ui/Card';
+import {Card, CardHeader, CardMedia, CardTitle} from 'material-ui/Card';
 import {GridList, GridTile} from 'material-ui/GridList';
 import CircularProgress from 'material-ui/CircularProgress';
 
@@ -23,17 +23,20 @@ export class WeatherWidget extends PureComponent {
           <CardHeader
             title="Current weather"
           />
-          Weather Widget
+
           {!gotWeather ? <CircularProgress size={80} thickness={5} /> : null}
           {this.props.weather.map((weather) => (
-            <GridTile
-              key={weather.get('id')}
-              title={weather.get('temp')}
-            >
-
-            <i className={weather.get('modernIcon')}></i>
-              <img src={weather.get('icon')} />
-            </GridTile>
+            <CardMedia key={weather.get('id')}>
+              <div className="weather-box">
+                <div className="weather-box__right">
+                  <i className={weather.get('modernIcon')} />
+                  <span className="weather-box__temp">{weather.get('temp')}</span>
+                  <span className="weather-box__localization">{weather.get('localization')}</span>
+                  <span className="weather-box__pressure">{weather.get('pressure')}</span>
+                </div>
+              </div>
+              <img className="" src="http://cdn.wallpapersafari.com/71/36/JPEBlv.jpg" alt="" />
+            </CardMedia>
           ))}
         </Card>
       </div>
